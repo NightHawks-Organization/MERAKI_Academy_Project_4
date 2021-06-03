e1ecf9500f049f8a863ce42b57c7faa2f9ca1a1a
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-06-03 21:42:37 +0300

    specilties schema done

--- backend/db/models/specialties .js
+++ backend/db/models/specialties .js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const specialties  = new mongoose.Schema({
     name : { type: String, required: true },
-    internalId: { type: mongoose.Schema.Types.ObjectId , ref:"Doctors" },
+    internalId: Number
     
 });
 
