a590db9009512b99b0253b34385f714f0da7c86e
Obada Alkhalaileh <81578973+ObadaAlkhalaileh@users.noreply.github.com>
2021-06-03 21:47:00 +0400

    updated
    trial to edit pull request

--- backend/db/models/specialties .js
+++ backend/db/models/specialties .js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-
-const specialties  = new mongoose.Schema({
+const specialties = new mongoose.Schema({
     name : { type: String, required: true },
     internalId: { type: mongoose.Schema.Types.ObjectId , ref:"Doctors" },
     
 });
 
-const insurance   = new mongoose.Schema({
+const insurance  = new mongoose.Schema({
     name : { type: String, required: true },
     
 });
@@ -15,7 +14,6 @@ const insurance   = new mongoose.Schema({
 const Specialties1 = mongoose.model("Specialties", specialties);
 const insurance1 = mongoose.model("Insurance", insurance);
 
-
 module.exports.Specialties = Specialties1;
 module.exports.Insurance = insurance1;
-
+//updated
