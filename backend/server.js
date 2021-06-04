8249c6561a0527f9db0f6e59ed75538105faa053
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-06-04 22:05:35 +0400

    edited register ticket

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const db = require('./db/db');
 const app = express();
 
 //routers
+const usersRouter = require("./routers/routes/auth/signUp");
 
 //built-in middlewares
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 //app routers
+app.use("/register", usersRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
