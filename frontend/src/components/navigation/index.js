d896566709d63682f61c0cdc91e22f16dd4557e4
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-06-08 01:01:02 +0400

    formatting css2

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -6,12 +6,12 @@ import './navigation.css'
 const Navigation = () => {
   return (
     <div className="navigation">
-      <Link to="/register" ><div className="link">Register</div></Link>
-      <Link to="/login" ><div className="link">Login</div></Link>
-      <Link to="/clinics" ><div className="link">Clinics</div></Link>
-      <Link to="/doctors" ><div className="link">Doctors</div></Link>
-      <Link to="/appointments" ><div className="link">Appointments</div></Link>
-      <Link to="/schedule" ><div className="link">Schedule</div></Link>
+      <Link to="/register" style={{textDecoration:"none"}} ><div className="link">Register</div></Link>
+      <Link to="/login" style={{textDecoration:"none"}}><div className="link">Login</div></Link>
+      <Link to="/clinics" style={{textDecoration:"none"}}><div className="link">Clinics</div></Link>
+      <Link to="/doctors" style={{textDecoration:"none"}}><div className="link">Doctors</div></Link>
+      <Link to="/appointments" style={{textDecoration:"none"}}><div className="link">Appointments</div></Link>
+      <Link to="/schedule" style={{textDecoration:"none"}}><div className="link">Schedule</div></Link>
     </div>
   );
 };
