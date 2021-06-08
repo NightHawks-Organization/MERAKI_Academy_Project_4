a40f3bac69e647c8307152f906775f9a9083fa4f
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-06-08 19:17:18 +0400

    finializing

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -6,11 +6,13 @@ import './main.css'
 import Navigation from "../navigation/index";
 import Dashboard from "../dashboard/index";
 import SignUp from "../auth/signUp";
+import Clinics from "../clinics/index";
 
 const Main = () => {
 	return <div className="main">
 		<Navigation/>
 		<Route path="/Register" render={()=><SignUp/>} />
+		<Route path="/clinics" render={()=><Clinics/>} />
 		{/* className="login" token={token} setToken={setToken} */}
 		{/* <Dashboard/> */}
 	</div>;
