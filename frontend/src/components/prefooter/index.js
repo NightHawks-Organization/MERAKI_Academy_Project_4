03dfedfbb5ceb141b5aed069991a78e8f19da198
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-06-10 00:45:55 +0300

    prefooter

--- frontend/src/components/prefooter/index.js
+++ frontend/src/components/prefooter/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import './prefooter.css'
+import "./prefooter.css";
 
-const preFooter = () => {
-	return <div className="prefooter">Prefooter component</div>;
+const PreFooter = () => {
+  return (
+    <div className="prefooter">
+      <div>
+     
+        <h1>Corona Virus(COVID-19)</h1>
+		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/800px-SARS-CoV-2_without_background.png"/>
+        <p>
+          Corona viruses are a group of related RNA viruses that cause diseases
+          in mammals and birds. In humans and birds, they cause respiratory
+          tract infections that can range from mild to lethal. Mild illnesses in
+          humans include some cases of the common cold (which is also caused by
+          other viruses, predominantly rhinoviruses), while more lethal
+          varieties can cause SARS, MERS, and COVID-19. In cows and pigs they
+          cause diarrhea, while in mice they cause hepatitis and
+          encephalomyelitis.
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default preFooter;
+export default PreFooter;
