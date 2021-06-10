04dc26a3e7ac91f9aea7bf55299b9fe6ddf8fe16
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-06-11 02:54:10 +0400

    get doctors and add doctors

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -9,6 +9,9 @@ import SignUp from "../auth/signUp";
 import LogIn from "../auth/login";
 import Clinics from "../clinics/index";
 import Appointments from "../appointments/index";
+import Doctors from "../DoctorsList/DoctorsList";
+import AddDoctor from "../addDoctor/addDoctor";
+
 
 const Main = ({token,setToken,role,setRole}) => {
     const [userId,setUserId]=useState()
@@ -19,6 +22,8 @@ const Main = ({token,setToken,role,setRole}) => {
 		<Route path="/login" render={()=><LogIn token={token} setToken={setToken} role={role} setRole={setRole} setUserId={setUserId}/>} />
 		<Route path="/clinics" render={()=><Clinics/>} />
 		<Route path="/appointments" render={()=><Appointments userId={userId} role={role}/>} />
+		<Route path='/doctors' render={()=><Doctors token={token}/>} />
+		<Route path='/addDoctor' render={()=><AddDoctor token={token} role={role}/>} />
 		{/* className="login" token={token} setToken={setToken} */}
 		{/* <Dashboard/> */}
 	</div>;
