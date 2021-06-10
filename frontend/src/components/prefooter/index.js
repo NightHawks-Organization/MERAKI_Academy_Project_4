3e17e15d7e512008e365c73884509330f63f166c
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-06-10 18:21:06 +0300

    prefooter

--- frontend/src/components/prefooter/index.js
+++ frontend/src/components/prefooter/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-
 import "./prefooter.css";
 
 const PreFooter = () => {
   return (
     <div className="prefooter">
-      <div>
+      <div className ="corona">
      
         <h1>Corona Virus(COVID-19)</h1>
 		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/800px-SARS-CoV-2_without_background.png"/>
@@ -20,7 +19,45 @@ const PreFooter = () => {
           encephalomyelitis.
         </p>
       </div>
-    </div>
+	  <div>
+	  <h1>what is a healthy, balanced diet for diabetes?</h1>
+	  <img src="https://1.bp.blogspot.com/-PCyUAlI0EKo/XvrZcnLDlhI/AAAAAAAABHs/u-6kw6uxzNs87tZ-s-8yDGiu_vZzcIubQCK4BGAsYHg/s635/diabetes-plate-method-medium.png"/>
+	  <p>
+	  There is no specific diet for diabetes. But the foods you eat not only make a difference 
+	  to how you manage your diabetes,
+	   but also to how well you feel and how much energy you have
+	   <h3>Eating from the main food groups </h3>
+	   <ul className="list">
+  <li>Fruit and veg</li>
+  <li>Starchy foods, like bread, pasta and rice</li>
+  <li>Protein foods, like beans, pulses, nuts, eggs, meat and fish</li>
+  <li>Starchy foods, like bread, pasta and rice</li>
+  <li>Dairy and alternatives</li>
+  <li>Oils and spreads</li>
+
+
+</ul> 
+        </p>
+	  </div>  
+	  <div className ="corona">
+     
+        <h1>Sports & Cardio Health</h1>
+		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/800px-SARS-CoV-2_without_background.png"/>
+        <p>
+		The heart is a muscular organ that pumps blood throughout the body, supplying oxygen and
+		 nutrients to the tissues while removing carbon dioxide and waste. When a person exercises, 
+		 the heart rate increases due to the heart pumping harder to send more oxygen (via the blood) 
+		 to the surrounding muscles at work. This results in increased blood flow, and also an increase
+		  in the blood volume returning to the heart. With regular exercise over time, the heart adapts 
+		  to the increased blood flow and the left heart becomes enlarged. This allows the left heart to
+		   hold more blood and more blood is delivered with each heartbeat, even during rest. Hence, the 
+		   resting heart rate of a sporty individual is lower as each beat delivers a larger volume of blood,
+		    and fewer beats are needed.
+
+
+        </p>
+      </div> 
+	   </div>
   );
 };
 
